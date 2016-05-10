ed8a41c964ec476f68b2c24ef8fbfb15ec5d2017
sallymander19 <karennilson@live.com>
2016-05-09 21:11:11 -0500

    tonights homework

--- src/main/java/edu/htc/gamedata/services/ReviewerController.java
+++ src/main/java/edu/htc/gamedata/services/ReviewerController.java
@@ -1,5 +1,6 @@
 package edu.htc.gamedata.services;
 
+import edu.htc.gamedata.entities.Review;
 import edu.htc.gamedata.entities.Reviewer;
 import edu.htc.gamedata.repositories.ReviewerRepository;
 import org.apache.log4j.Logger;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ReviewerController {
@@ -24,5 +27,17 @@ public class ReviewerController {
     public Reviewer findReviewer(@RequestParam(value="username") String userName) {
         Reviewer reviewer =  reviewerRepository.findOne(userName);
         return reviewer;
+    }
+
+    @RequestMapping("/search/reviewerByGender")
+    public List<Reviewer> findReviewerByGender(@RequestParam(value="gender")String gender){
+        List<Reviewer> reviewers = reviewerRepository.findByGender(gender);
+        return reviewers;
+    }
+
+    @RequestMapping("/search/reviewerByAge")
+    public List<Reviewer> findByAge(@RequestParam(value ="age")int age) {
+        List<Reviewer> reviewers = reviewerRepository.findByAge(age);
+        return reviewers;
     }
 }
