9109d1be252f3b842b0ded05ede97fd55c03e083
JoeFranks1993 <Josephdeanfranks@gmail.com>
2016-05-10 20:27:33 -0500

    Add files via upload

--- src/main/java/edu/htc/gamedata/repositories/ReviewerRepository.java
+++ src/main/java/edu/htc/gamedata/repositories/ReviewerRepository.java
@@ -1,9 +1,24 @@
 package edu.htc.gamedata.repositories;
 
 import edu.htc.gamedata.entities.Reviewer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface ReviewerRepository extends CrudRepository<Reviewer, String> {
+
+
+    List<Reviewer> findByName(String name);
+
+
+
+    @Query("SELECT r FROM Reviewer r WHERE r.gender = :gender")
+    List<Reviewer> findReviewerByGender(@Param("gender") String gender);
+
+    @Query("SELECT r FROM Reviewer r")
+    List<Reviewer> findAllReviewers();
 
 }
